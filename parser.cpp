b382d93eab011725f046fb055024f8a947991a07
mariashka <bisyarinamariashka@gmail.com>
2016-03-22 14:55:42 +0300

    Debug output removed

--- parser.cpp
+++ parser.cpp
@@ -16,10 +16,8 @@ tree parser::VPrime(){
   tree t("V'");
   if (l.cur().type == token_type::whitespace) {
     l.next();
-    cout << "VPrime (B)" << endl;
     return t;
   }
-  cout << "VPrime ()" << endl;
   return t;
 }
   
@@ -30,19 +28,16 @@ tree parser::V(){
     return V();
   }
   if (l.cur().type == token_type::deref) {
-    cout << "V (*, V)" << endl;
     t.add_child(tree("*"));
     l.next();
     t.add_child(V());
     return t;
   }
   if (l.cur().type == token_type::name) {
-    cout << "V (N, VPrime)" << endl;
     t.add_child(N());
     t.add_child(VPrime());
     return t;
   }
-  cout << "V fail" << endl;
   throw l.cur();
 }
 
@@ -51,23 +46,19 @@ tree parser::LPrime(){
   if (l.cur().type == token_type::comma) {
     t.add_child(tree(","));
     l.next();
-    cout << "LPrime (, , L)" << endl;
     t.add_child(L());
     return t;
   }
-  cout << "LPrime ()" << endl;
   return t;
 }
 
 tree parser::L(){
   tree t("L");
   if (l.cur().type == token_type::name || l.cur().type == token_type::whitespace || l.cur().type == token_type::deref) {
-    cout << "L (V, LPrime)" << endl;
     t.add_child(V());
     t.add_child(LPrime());
     return t;
   }
-  cout << "L fail" << endl;
   throw l.cur();
 }
 
@@ -75,10 +66,8 @@ tree parser::N(){
   if (l.cur().type == token_type::name) {
     tree t("N: " + l.cur().s);
     l.next();
-    cout << t.s << endl;
     return t;
   }
-  cout << "N fail" << endl;
   throw l.cur();
 }
   
@@ -87,12 +76,10 @@ tree parser::D(){
   if (l.cur().type == token_type::whitespace) {
     l.next();
   }
-  cout << "D (N, L, ;)" << endl;
   t.add_child(N());
   if (l.cur().type == token_type::whitespace) {
     l.next();
   } else { 
-    cout << "D fail" << endl;
     throw l.cur();
   }
   t.add_child(L());
@@ -100,7 +87,6 @@ tree parser::D(){
     t.add_child(tree(";"));
     l.next();
   } else { 
-    cout << "D fail" << endl;
     throw l.cur();
   }
   return t;
@@ -109,15 +95,12 @@ tree parser::D(){
 tree parser::S() {
   if (l.cur().type == token_type::end) {
     return tree("S");
-    cout << "S ()" << endl;
   }
   if (l.cur().type == token_type::name || l.cur().type == token_type::whitespace) {
-    cout << "S (D, S)" << endl;
     tree t("S");
     t.add_child(D());
     t.add_child(S());
     return t;
   }
-  cout << "S fail" << endl;
   throw l.cur();
 }
