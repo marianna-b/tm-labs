3112b98fcbe6698d4cfe690ca3ead033eca4e17b
mariashka <bisyarinamariashka@gmail.com>
2016-05-07 20:49:56 +0300

    Added first and follow computing

--- lab4/gen.cpp
+++ lab4/gen.cpp
@@ -1,10 +1,12 @@
 #include "gen.h"
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 
 rule::rule(vector<string> t, string c) {
   terms = t;
+  reverse(terms.begin(), terms.end());
   code = c;
 }
 
@@ -23,36 +25,185 @@ parsed_info::parsed_info(string* b, string* e, map<string, string>* tok, map<str
   token = tok;
   nonterm = nont;
   grammar = gr;
+  gen_first();
+  gen_follow();
 }
 
 string parsed_info::generate() {
   string s = "";
-  s += *begin;
-  s += "\n\n";
-  s += *end;
-  s += "\n\n";
+  s += *begin + "\n";
+  s += *end + "\n";
 
-  for (auto i : (*token)) {
+  for (auto i : (*token)) 
     s += i.first + " " + i.second + "\n";
-  }
-  s += "\n\n";
-  for (auto i : (*nonterm)) {
+  for (auto i : (*nonterm)) 
     s += i.first + " " + i.second + "\n";
-  }
-  s += "\n\n";
-  s += "\n\n";
   for (auto i : (*grammar)) {
     s += i.first + "\n";
-    for (auto j : i.second) {
+    for (auto j : i.second) 
       s += j.show() + "\n";
-    }
-    s += "\n\n";
   }
+
+  s += "\n\n\n";
+
+  for (auto i : first) {
+    s += i.first + "\n";
+    for (auto j : i.second) 
+      if (j != "")
+        s += j + "\n";
+      else
+        s += "eps\n"; 
+  }
+
+  s += "\n";
+
+  for (auto i : follow) {
+    s += i.first + "\n";
+    for (auto j : i.second) 
+      s += j + "\n";
+  }
+  
   return s;
 }
 
-void parsed_info::generate(string, string) {
- 
+bool is_token(string a) {
+  if (a == "")
+    return false;
+  if (a[0] <= 'Z' && a[0] >= 'A')
+    return true;
+  if (a == "$")
+    return true;
+  return false;
+}
+
+bool parsed_info::add_firsts(string name, rule& r, int idx) {
+  string a = r.terms[idx];
+  bool changed = false;
+  bool eps = false;
+
+  if (is_token(a)) {
+    if (first[name].count(a) == 0) {
+      first[name].insert(a);
+      return true;
+    }
+    return false;
+  } 
+  for (auto f : first[a]) {
+    if (f != "") {
+      if (first[name].count(f) == 0) {
+        changed = true;
+        first[name].insert(f);
+      }
+    } else {
+      eps = true;
+    }
+  }
+  if (eps) {
+    if (idx + 1 < (int)r.terms.size()) {
+      changed |= add_firsts(name, r, idx + 1);
+    } else {
+      if (first[name].count("") == 0) {
+        changed = true;
+        first[name].insert("");
+      }
+    }
+  }
+  return changed;
+}
+
+
+void parsed_info::gen_first() {
+  bool changed = true;
+  while (changed) {
+    changed = false;
+    for (auto g : (*grammar)) {
+      string name = g.first;
+      for (auto rule : g.second) {
+        if (rule.terms.empty()) {
+          if (first[name].count("") == 0) {
+            changed = true;
+            first[name].insert("");
+          }
+          continue;
+        }
+        changed = add_firsts(name, rule, 0);
+      }
+    }
+  }
+}
+
+void parsed_info::gen_follow() {
+  follow["start"].insert("$");
+  bool changed = true;
+
+  while (changed) {
+    changed = false;
+    for (auto g : (*grammar)) {
+      string name = g.first;
+      
+      for (auto rule : g.second) {
+        for (int i = 0; i < (int)rule.terms.size(); ++i) {
+          if (is_token(rule.terms[i])) {
+            continue;
+          }
+          int j = i + 1;
+          for (j; j < (int)rule.terms.size(); ++j) {
+            if (is_token(rule.terms[j]))
+              break;
+            if (follow[rule.terms[j]].count("") == 0) 
+              break;
+          }
+          if (j == (int)rule.terms.size()) {
+            for (auto elem : follow[name]) {
+              if (follow[rule.terms[i]].count(elem) == 0) {
+                follow[rule.terms[i]].insert(elem);
+                changed = true;
+              }
+            }
+          }
+          if (i + 1 == (int)rule.terms.size()) {
+            continue;
+          }
+          changed |= add_follows(rule.terms[i], rule, i + 1);
+        }
+      }
+    }
+  }
+}
+
+bool parsed_info::add_follows(string name, rule& r, int idx) {
+  bool changed = false;
+  bool eps = false;
+  if (is_token(r.terms[idx])) {
+    if (follow[name].count(r.terms[idx]) == 0) {
+      follow[name].insert(r.terms[idx]);
+      changed = true;
+    }
+    return changed;
+  }
+  for (auto f : first[r.terms[idx]]) {
+    if (f != "") {
+      if (follow[name].count(f) == 0) {
+        follow[name].insert(f);
+        changed = true;
+      }
+    } else {
+      eps = true;
+    }
+  }
+  if (eps) {
+    if (idx + 1 < (int)r.terms.size())
+      changed |= add_follows(name, r, idx + 1);
+  }
+  return changed;
+}
+
+string parsed_info::generate_file() {
+  return "";
+}
+
+string parsed_info::generate_header() {
+  return "";
 }
   
 parsed_info::~parsed_info() {
--- lab4/gen.h
+++ lab4/gen.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <set>
 
 struct rule {
   std::vector<std::string> terms;
@@ -15,14 +16,21 @@ struct rule {
 struct parsed_info {
   parsed_info(std::string*, std::string*, std::map<std::string, std::string>*, std::map<std::string, std::string>*, std::map<std::string, std::vector<rule> >*);
 
-  void generate(std::string, std::string);
   std::string generate();
+  std::string generate_file();
+  std::string generate_header();
   
   ~parsed_info();
 private:
   std::string *begin, *end;
   std::map<std::string, std::string> *token, *nonterm;
   std::map<std::string, std::vector<rule> > *grammar;
+  std::map<std::string, std::set<std::string> > first;
+  std::map<std::string, std::set<std::string> > follow;
+  void gen_first();
+  void gen_follow();
+  bool add_firsts(std::string, rule&, int);
+  bool add_follows(std::string, rule&, int);
 };
  
 
